66e2d0c8947617e0b497c87f2db0b01cb424db23
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2022-12-06 15:48:14 +0100

    feat: add subject exercise

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Observer } from "rxjs";
-
-const observer: Observer<any> = {
-  next: (value) => console.log("next:", value),
-  error: (error) => console.warn("error:", error),
-  complete: () => console.info("completado"),
-};
