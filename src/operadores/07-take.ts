a58a00917a964d764122357b53ed1736a8ef6382
Eloy Ortiz <eloy.ortiz@sepalo.es>
2023-01-04 21:45:46 +0100

    feat: add take exercise

--- src/operadores/07-take.ts
+++ src/operadores/07-take.ts
@@ -1,7 +1,14 @@
 import { of, take, tap } from "rxjs";
 
+const numeros$ = of(1,2,3,4,5);
+
 const observer = {
   next: (val) => console.log("next", val),
   complete: () => console.log("complete"),
 };
 
+numeros$.pipe(
+  tap( t => console.log('tap', t)),
+  take(3)
+)
+.subscribe( observer);
