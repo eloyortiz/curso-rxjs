d3eff711423bb02bcfcaf942db37bca39a4936f4
Eloy Ortiz <eloy.ortiz@sepalo.es>
2023-01-05 12:20:29 +0100

    feat: add sampleTime exercise

--- src/operadores-tiempo/03-sampleTime.ts
+++ src/operadores-tiempo/03-sampleTime.ts
@@ -1,3 +1,4 @@
+import { fromEvent, map, sampleTime } from 'rxjs';
 
 
 const observer = {
@@ -5,3 +6,10 @@ const observer = {
   complete: () => console.log("complete"),
 };
 
+const click$ = fromEvent<MouseEvent>(document, 'click');
+
+click$.pipe(
+  sampleTime(2000),
+  map( ({x,y}) => ({x,y})),
+)
+.subscribe(observer);
