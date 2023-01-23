9b233d616c2e5a1e382186fa9d0c6644c60eb869
Eloy Ortiz Pulido <eortizpu@emeal.nttdata.com>
2023-01-23 17:05:30 +0100

    feat: add ajax-catchError exercise

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,43 @@
-
+import { catchError, map, of } from "rxjs";
+import { ajax, AjaxError } from "rxjs/ajax";
 
 const observer = {
   next: (val) => console.log("next", val),
   complete: () => console.log("complete"),
 };
 
+const url = "https://api.github.com/users?per_page=5";
+
+const manejaErrores = (response: Response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+};
+
+const atrapaError = (err: AjaxError) => {
+  console.warn("error en", err.message);
+  return of([]);
+};
+
+const fetchPromesa = fetch(url);
+
+// fetchPromesa
+//   .then((resp) => resp.json())
+//   .then((data) => console.log("data: ", data))
+//   .catch((err) => console.warn("error en usuario", err));
+
+// fetchPromesa
+//   .then(manejaErrores)
+//   .then((resp) => resp.json())
+//   .then((data) => console.log("data: ", data))
+//   .catch((err) => console.warn("error en usuario", err));
+
+ajax(url)
+  .pipe(
+    //pluck("response"), //DEPRECATED
+    map((resp) => resp.response),
+    catchError(atrapaError)
+  )
+  .subscribe((users) => console.log("usuarios: ", users));
