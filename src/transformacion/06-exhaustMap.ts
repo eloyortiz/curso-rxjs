6c5b734ec7759285439d72a0284eaa9ae3dec7c9
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2023-01-25 14:23:01 +0100

    feat: add exhaustMap exercise

---
+++ src/transformacion/06-exhaustMap.ts
@@ -0,0 +1,8 @@
+import { concatMap, exhaustMap, fromEvent, interval, take } from "rxjs";
+
+const interval$ = interval(500).pipe(take(3));
+const click$ = fromEvent(document, "click");
+
+click$.pipe(
+    exhaustMap(() => interval$)
+).subscribe(console.log);
